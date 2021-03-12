ff98b66d16e92a7200e0c472fa70bba342e935de
LegoYoda112 <thomas.godden@outlook.com>
2021-03-12 11:36:31 +0000

    New 3D interface, filter on derivative components, fixed doubled pitch in commands

--- arduino/3D_controller_V1/3D_controller_V1.ino
+++ arduino/3D_controller_V1/3D_controller_V1.ino
@@ -64,9 +64,9 @@ Publisher wheelA = Publisher("wheelA");
 Publisher wheelB = Publisher("wheelB");
 Publisher wheelC = Publisher("wheelC");
 
+// Set up subscribers
 SubscriberManager sm;
 
-// Set up subscribers
 Subscriber onoff_sub = Subscriber("on");
 Subscriber kP_sub = Subscriber("kP");
 Subscriber kD_sub = Subscriber("kD");
@@ -74,17 +74,16 @@ Subscriber kOffset_sub = Subscriber("kOffset");
 Subscriber cutoffAngle_sub = Subscriber("cutoffAngle");
 Subscriber roll_setpoint_sub = Subscriber("rollSP");
 Subscriber pitch_setpoint_sub = Subscriber("pitchSP");
+Subscriber yaw_setpoint_sub = Subscriber("yawSP");
 
 // ================================== SETUP
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 
   // init gyro
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
-
 
   // Assign publishers and subscribers
   pm.addPublisher(time_pub);
@@ -95,7 +94,6 @@ void setup() {
   pm.addPublisher(wheelB);
   pm.addPublisher(wheelC);
 
-
   sm.addSubscriber(onoff_sub);
   sm.addSubscriber(kP_sub);
   sm.addSubscriber(kD_sub);
@@ -103,35 +101,50 @@ void setup() {
   sm.addSubscriber(cutoffAngle_sub);
   sm.addSubscriber(roll_setpoint_sub);
   sm.addSubscriber(pitch_setpoint_sub);
+  sm.addSubscriber(yaw_setpoint_sub);
 }
 
-// ================================== OTHER VARS
-unsigned long loop_time_ms = 50; // ms
+// ================================== OTHER VARS / CONSTANTS
+
+// Loop timing
+unsigned long loop_time_ms = 30; // ms
 double loop_time_s = loop_time_ms / 1000.0; // seconds!
 unsigned long start_time_ms;
 
+// Motor value limit
+const double MAX_POWER = 1;
+
 // Pre-computed sin cos values
-double s = 0.866025403784; // sin(120)
-double c = -0.5; // cos(120)
+const double s = 0.866025403784; // sin(120)
+const double c = -0.5; // cos(120)
 
 // Pre-computed deg -> rad
-double degtorad = 0.01745329;
+const double degtorad = 0.01745329;
 
+// Roll-Pitch-Yaw commanded forces/torques
 double rollCmd = 0;
 double pitchCmd = 0;
+double yawCmd = 0;
 
+// Setpoint offsets
 double rollOffset = 0;
 double pitchOffset = 0;
-
-const double MAX_POWER = 0.5;
 
 // Roll Pitch Yaw values
 double prev_roll_val = 0;
 double roll_dot = 0;
+double roll_dot_filtered = 0;
+
 double prev_pitch_val = 0;
 double pitch_dot = 0;
+double pitch_dot_filtered = 0;
+
 double prev_yaw_val = 0;
 double yaw_dot = 0;
+double yaw_dot_filtered = 0;
+
+// IIR filter alpha
+const double alpha = 0.2;
 
 // ================================== LOOOOOOP
 void loop() {
@@ -147,18 +160,25 @@ void loop() {
   mpu6050.update();
   roll.value = mpu6050.getAngleX() * degtorad; // Roll
   pitch.value = mpu6050.getAngleY() * degtorad; // Pitch
-  yaw.value = mpu6050.getAngleZ() * degtorad;
+  yaw.value = mpu6050.getAngleZ() * degtorad - yaw_setpoint_sub.value; // Yaw
 
   // Update derivatives
   roll_dot = (roll.value - prev_roll_val) / loop_time_s;
   pitch_dot = (pitch.value - prev_pitch_val) / loop_time_s;
   yaw_dot = (yaw.value - prev_yaw_val) / loop_time_s;
 
-  // PD on roll and pitch
-  rollCmd = kP_sub.value * (roll.value - roll_setpoint_sub.value + rollOffset) + kD_sub.value * (roll_dot);
-  pitchCmd = kP_sub.value * (pitch.value - pitch_setpoint_sub.value + pitchOffset) + kD_sub.value * (pitch_dot);
+  // Use an IIR filter to perform a basic lowpass
+  // Attempts to cutout noise due to wheel vibrations
+  roll_dot_filtered = alpha * roll_dot + (1 - alpha) * roll_dot_filtered;
+  pitch_dot_filtered = alpha * pitch_dot + (1 - alpha) * pitch_dot_filtered;
+  yaw_dot_filtered = alpha * yaw_dot + (1 - alpha) * yaw_dot_filtered;
 
-  // Offset
+  // PD on roll, pitch and yaw
+  rollCmd = constrain(kP_sub.value * (roll.value + rollOffset - roll_setpoint_sub.value) + kD_sub.value * (roll_dot_filtered), -1.5, 1.5);
+  pitchCmd = constrain(kP_sub.value * (pitch.value + pitchOffset - pitch_setpoint_sub.value) + kD_sub.value * (pitch_dot_filtered), -1.5, 1.5);
+  //yawCmd = kP_sub.value * (yaw.value) + kD_sub.value * yaw_dot_filtered;
+
+  // Offsets
   rollOffset = rollOffset * onoff_sub.value;
   rollOffset += kOffset_sub.value * rollCmd * loop_time_s;
 
@@ -167,12 +187,16 @@ void loop() {
 
 
   // Calculate wheel powers
-  wheelA.value = rollCmd * c + pitchCmd * s; // Wheel A power
-  wheelB.value = -rollCmd; // Wheel B power
-  wheelC.value = rollCmd * c - pitchCmd * s; // Wheel C power
+  wheelA.value = - rollCmd * c + pitchCmd * s; // Wheel A power
+  wheelB.value = - rollCmd; // Wheel B power
+  wheelC.value = - rollCmd * c - pitchCmd * s; // Wheel C power
 
+  // Safety cutoff
+  if(abs(roll.value - roll_setpoint_sub.value) > 0.3 || abs(pitch.value - pitch_setpoint_sub.value) > 0.3){
+    onoff_sub.value = 0;
+  }
 
-  // Set constrained values if on = 1
+  // Set constrained motor values if on = 1
   if(onoff_sub.value == 1){
     motorA.setPower(constrain(wheelA.value, -MAX_POWER, MAX_POWER));
     motorB.setPower(constrain(wheelB.value, -MAX_POWER, MAX_POWER));
--- javascript/static/main.js
+++ javascript/static/main.js
@@ -1,3 +1,4 @@
+//const SerialPort = require("serialport");
 
 // Helpter functions
 const el = (id) => document.getElementById(id);
@@ -10,12 +11,16 @@ const controlList = el("controls");
 
 var scene, renderer, camera;
 var cube;
-var cube_x_angle = 0;
+var cubeLine, wheelALine, wheelBLine, wheelCLine;
+var cubeLineDir;
 
-let saved_data;
+let offsetPitch = 0;
+let offsetRoll = 0;
+
+let saved_data = {};
 
 const control_names = ["kP", "kD", "kOffset"];
-const default_values = [-2, -0.5, -0.1];
+const default_values = [-60, -1, -0.03];
 const send_button = el("send_button");
 const on_button = el("on_button");
 const off_button = el("off_button");
@@ -43,14 +48,15 @@ window.addEventListener("load", function () {
 
 
 // ======================= UPDATER =============================
-var angleObject =  {};
 let updateValue = setInterval(function () {
     getData().then(function (data){
         updatePlot(data);
-        angleObject = data;
-        cube_x_angle = -parseFloat(data.roll);
+        // angleObject = data;
 
         saved_data = data;
+        saved_data.roll = parseFloat(data.roll); // Go from text -> float
+        saved_data.pitch = parseFloat(data.pitch);
+        saved_data.yaw = parseFloat(data.yaw);
         //console.log(data);
     })
 }, 50);
@@ -125,8 +131,8 @@ function init(){
 
     // === SCENE
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xcccccc );
-    scene.fog = new THREE.FogExp2( 0xcccccc, 0.09);
+    scene.background = new THREE.Color( 0x20262E );
+    scene.fog = new THREE.FogExp2( 0x20262E, 0.09);
 
     // ==== RENDERER
 
@@ -145,17 +151,45 @@ function init(){
     // === CAMERA
     camera = new THREE.PerspectiveCamera( 60, scene_width/scene_height, 0.1, 1000 );
 
-    camera.position.z = 4;
+    camera.position.z = 3;
     camera.position.y = 3;
 
-    camera.rotation.x = -0.55;
+    camera.rotation.x = -0.7;
 
     // ==== GOEMETRY
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshPhongMaterial( { color: 0xff9900, flatShading: true } );
+    material.opacity = 0.7;
+    material.transparent = true;
     cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
+    // === Arrows and stuff
+    cubeLineDir = new THREE.Vector3(1, 2, 0);
+    cubeLineDir.normalize();
+
+    const origin = new THREE.Vector3(0, 0, 0);
+    const length = 2;
+
+    cubeLine = new THREE.ArrowHelper(cubeLineDir, origin, length, 0xFA6666, 0.2, 0.1);
+    scene.add(cubeLine);
+
+    const upLine = new THREE.ArrowHelper(new THREE.Vector3(0, 1, 0), origin, length, 0x81FA66, 0.2, 0.1);
+    scene.add(upLine);
+
+    const wheelADir = new THREE.Vector3(-0.5, 0, 0.866);
+    wheelALine = new THREE.ArrowHelper(wheelADir, origin, 1, 0x2279E3, 0.1, 0.3);
+    scene.add(wheelALine);
+
+    const wheelBDir = new THREE.Vector3(1, 0, 0);
+    wheelBLine = new THREE.ArrowHelper(wheelBDir, origin, 1, 0x34A33F, 0.1, 0.3);
+    scene.add(wheelBLine);
+
+    const wheelCDir = new THREE.Vector3(-0.5, 0, -0.866);
+    wheelCLine = new THREE.ArrowHelper(wheelCDir, origin, 1, 0xE3A322, 0.1, 0.3);
+    scene.add(wheelCLine);
+
+    
     // === Ground Grid
 
     const size = 20;
@@ -170,18 +204,34 @@ function init(){
 
     const ambientLight = new THREE.AmbientLight( 0x222222 );
     scene.add( ambientLight );
+
 }
 
 var animate = function () {
     requestAnimationFrame( animate );
     
 	//cube.rotation.x = parseFloat(angleObject.Num)/10;
-    //cube.rotation.y = 0;
-    cube.rotation.z = cube_x_angle + 3.1415/4;
+    cube.rotation.x = -saved_data.pitch + offsetPitch + 3.1415/4; // Pitch
+    cube.rotation.z = -saved_data.roll + offsetRoll + 3.1415/4; // Roll
 
-    cube.position.x = Math.cos(cube.rotation.z + 3.1415/4)/Math.sqrt(2)
-    cube.position.y = Math.sin(cube.rotation.z + 3.1415/4)/Math.sqrt(2)
-    cube.position.z = -0.5;
+    cube.position.x = Math.cos(cube.rotation.z + 3.1415/4)* (0.5);
+    cube.position.y = Math.sin(cube.rotation.z + 3.1415/4)* (0.5);
+
+    cube.position.y = cube.position.y * Math.cos(cube.rotation.x - 3.1415/4);
+    cube.position.z = cube.position.y * Math.sin(cube.rotation.x - 3.1415/4);
+
+    //cube.position.y += 0.1;
+
+    var dir = new THREE.Vector3(cube.position.x, cube.position.y, cube.position.z).normalize()
+    //cubeLine.setDirection(dir);
+
+    cubeLine.rotation.x = (-saved_data.pitch + offsetPitch);
+    cubeLine.rotation.z = (-saved_data.roll + offsetRoll);
+
+    const powerScale = 1;
+    wheelALine.setLength(parseFloat(saved_data.wheelA)/powerScale, 0.3, 0.3);
+    wheelBLine.setLength(parseFloat(saved_data.wheelB)/powerScale, 0.3, 0.3);
+    wheelCLine.setLength(parseFloat(saved_data.wheelC)/powerScale, 0.3, 0.3);
 
 	renderer.render( scene, camera );
 };
@@ -246,8 +296,11 @@ off_button.onclick = function () {
 
 calibrate_button.onclick = function() {
     console.log("Calibrating offsets");
+    offsetRoll = saved_data.roll;
+    offsetPitch = saved_data.pitch;
     sendData({rollSP: saved_data.roll});
     sendData({pitchSP: saved_data.pitch});
+    sendData({yawSP: saved_data.yaw});
 }
 
 
